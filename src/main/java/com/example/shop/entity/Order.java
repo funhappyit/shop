d593a501e2afa3e6f569681d1e37264bab3d9c12
“funhappyit” <fun.happyit@gmail.com>
2024-04-18 22:43:57 +0900

    2024-04-19 스프링 시큐리티 다대일, 일대다 양방향 매핑

---
+++ src/main/java/com/example/shop/entity/Order.java
@@ -0,0 +1,32 @@
+package com.example.shop.entity;
+
+import com.example.shop.constant.OrderStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="orders")
+@Getter @Setter
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name="order_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="member_id")
+    private Member member;
+
+    private LocalDateTime orderDate; //주문일
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus; //주문상태
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
+}
