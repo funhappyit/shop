ab412ff644e20fd5aea98101aaad7884e7343313
“funhappyit” <fun.happyit@gmail.com>
2024-04-22 23:08:39 +0900

    2024-04-22 1) 장바구니 기능 커밋및 푸시

--- src/main/java/com/example/shop/service/CartService.java
+++ src/main/java/com/example/shop/service/CartService.java
@@ -1,5 +1,11 @@
 package com.example.shop.service;
 
+import com.example.shop.dto.CartItemDto;
+import com.example.shop.entity.Cart;
+import com.example.shop.entity.CartItem;
+import com.example.shop.entity.Item;
+import com.example.shop.entity.Member;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,6 +26,24 @@ public class CartService {
 	private final CartRepository cartRepository;
 	private final CartItemRepository cartItemRepository;
 
+	public Long addCart(CartItemDto cartItemDto,String email){
+		Item item = itemRepository.findById(cartItemDto.getItemId()).orElseThrow(EntityNotFoundException::new);
+		Member member = memberRepository.findByEmail(email);
 
+		Cart cart = cartRepository.findByMemberId(member.getId());
+		if(cart == null){
+			cart = Cart.createCart(member);
+			cartRepository.save(cart);
+		}
+		CartItem savedCartItem = cartItemRepository.findByCartIdAndItemId(cart.getId(),item.getId());
 
+		if(savedCartItem != null){
+			savedCartItem.addCount(cartItemDto.getCount());
+			return savedCartItem.getId();
+		}else {
+			CartItem cartItem = CartItem.createCartItem(cart,item,cartItemDto.getCount());
+			cartItemRepository.save(cartItem);
+			return cartItem.getId();
+		}
+	}
 }
